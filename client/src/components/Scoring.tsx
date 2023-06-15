b833785e84037db0bc73910b50c18d51adc3aea6
ruslan-s <32303191+ruslan-s@users.noreply.github.com>
2023-06-15 13:10:53 +0000

    introduce tts

--- client/src/components/Scoring.tsx
+++ client/src/components/Scoring.tsx
@@ -1,11 +1,13 @@
-import { FunctionComponent, ReactNode, useContext } from "react";
+import React, { FunctionComponent, ReactNode, useContext, useEffect, useState } from "react";
 import { Target32Regular } from "@fluentui/react-icons";
 import { Map, Marker } from "pigeon-maps";
 import styles from "../styles/Scoring.module.scss";
 import { LiveGameContext, ILiveGameContext } from "./LiveShareContextProvider";
 import { UserList } from "./UserList";
 import { MapProvider } from "../utils/MapProvider";
-import { TeamsContext } from "../components";
+import { ILiveGameUser, TeamsContext } from "../components";
+import { DistanceHelper } from "../utils";
+import { TTS } from "../utils/tts";
 
 type ScoringProps = {
 	// children: ReactNode,
@@ -14,22 +16,46 @@ type ScoringProps = {
 export const Scoring: FunctionComponent<ScoringProps> = () => {
 	const { question, userMap, tileProvider } = useContext(LiveGameContext) as ILiveGameContext;
 	const { teamsContext } = useContext(TeamsContext);
+	const [roundWinner, setRoundWinner] = useState<ILiveGameUser>();
+	const ttsService = new TTS();
+	const [ markers, setMarkers ] = useState<React.ReactNode>([]);
 
-	const markers: ReactNode[] = [];
-	userMap.forEach((user, key) => {
-		if (user.position) {
-			const colorMarker = (key === teamsContext?.user?.id) ? "var(--colorPaletteRedBackground3)" : "var(--colorNeutralForeground2)";
-			const markerPosition: [number, number] = [user.position?.lat, user.position?.lng];
-			markers.push(<Marker key={key} anchor={markerPosition} color={colorMarker} />);
+	useEffect(() => {
+		const mrks: ReactNode[] = [];
+		const userDistances = Array<{ user: ILiveGameUser, distance: number }>();
+		userMap.forEach((user, key) => {
+			if (user.position) {
+				const colorMarker = (key === teamsContext?.user?.id) ? "var(--colorPaletteRedBackground3)" : "var(--colorNeutralForeground2)";
+				const markerPosition: [number, number] = [user.position?.lat, user.position?.lng];
+				mrks.push(<Marker key={key} anchor={markerPosition} color={colorMarker} />);
+			}
+
+			const distance = (user.position && question) ?
+				DistanceHelper.getPositionDistance(user.position, question.location) : 0;
+			userDistances.push({ user, distance });
+		});
+
+		if (question) {
+			const targetPosition: [number, number] = [question.location.lat, question.location.lng];
+			mrks.push(<Marker key={"target"} anchor={targetPosition} color="red">
+				<div className={styles.targetMarker}><Target32Regular primaryFill="var(--colorPaletteRedBackground3)" /></div>
+			</Marker>);
 		}
-	});
 
-	if (question) {
-		const targetPosition: [number, number] = [question.location.lat, question.location.lng];
-		markers.push(<Marker key={"target"} anchor={targetPosition} color="red">
-			<div className={styles.targetMarker}><Target32Regular primaryFill="var(--colorPaletteRedBackground3)" /></div>
-		</Marker>);
-	}
+		setMarkers(mrks);
+
+		if (!!userDistances.length) {
+			const sorted = userDistances.sort((a, b) => a.distance - b.distance)[0];
+			if (sorted) {
+				setRoundWinner(sorted.user);
+			}
+		}
+
+	}, []);
+
+	useEffect(() => {
+		ttsService.announceRoundWinner(roundWinner?.name || '');
+	}, [roundWinner]);
 
 	return <div className={styles.scoringWrapper}>
 		<div className={styles.scoringLeft}>
